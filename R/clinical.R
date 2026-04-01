15db290e379925f97af8d43e90f0f401eef01e8f
liuzhy <43285689+liu-zhiyang@users.noreply.github.com>
2026-04-01 15:26:21 +0800

    Comment out renaming of disease_response
    Comment out the renaming of disease_response to follow_ups_disease_response.

--- R/clinical.R
+++ R/clinical.R
@@ -315,14 +315,14 @@ GDCquery_clinic <- function(
                             days_to_follow_up,
                             #days_to_recurrence,
                             #progression_or_recurrence_type,
-                            disease_response
+                            #disease_response
                         )
                     ) %>%
                     dplyr::filter(!is.na(submitter_id), !is.na(days_to_follow_up)) %>%
                     dplyr::group_by(submitter_id) %>%
                     dplyr::filter(dplyr::row_number() == which.max(days_to_follow_up)) %>%
                     dplyr::ungroup()  %>%
-                    dplyr::rename_at(dplyr::vars(disease_response),.funs = function(x) paste0("follow_ups_",x)) %>%
+                    #dplyr::rename_at(dplyr::vars(disease_response),.funs = function(x) paste0("follow_ups_",x)) %>%
                     dplyr::rename(days_to_last_follow_up = days_to_follow_up)
 
                 df <- dplyr::full_join(df, follow_ups_last, by = "submitter_id")
